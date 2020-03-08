ab23782e972212bd413f400723b8ca4bd20582b3
ARTHUR PC <arthurpimetelcordeiro@gmail.com>
2020-03-08 08:55:53 -0300

    exibindo repositório

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,13 @@
+/* eslint-disable react/no-typos */
+/* eslint-disable react/sort-comp */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import api from '../../services/api'
 
-// import { Container } from './styles';
+import Container from '../../components/Container'
+import { Loading, Owner } from './styles'
 
 export default class Repository extends Component {
   static PropTypes = {
@@ -45,6 +49,19 @@ export default class Repository extends Component {
   render() {
     const { repository, issues, loading } = this.state
 
-    return <h1>Repository</h1>
+    if (loading) {
+      return <Loading>Carregando</Loading>
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    )
   }
 }
